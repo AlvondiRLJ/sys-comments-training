cc661bf8dfc28c0b2f2a09afb09e59572fe8efa2
alvondirlj <a.junner@gmail.com>
2018-07-11 22:35:21 -0300

    add unit tests in Comment and Comments

--- src/Comment.js
+++ src/Comment.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
-const Comment = (props) => <div className='card'><p className='card-body'>{props.comment.comment} </p></div>
+const Comment = (props) => <div className='card'><p className='card-body'>{props.comment.comment}</p></div>
 
 export default Comment
 
 //const Comment = (props) => <div className='card'><p className='card-body'>{props.comment.comment} por: {props.comment.user.name}</p></div>
-
